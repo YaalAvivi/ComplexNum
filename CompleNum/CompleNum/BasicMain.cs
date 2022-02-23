38eacf6f91e26ed9c2b2cc666de3aa5088cfda8a
YaalLA <yaal.avivi@gmail.com>
2022-02-23 15:16:07 +0200

    Add files via upload

---
+++ CompleNum/CompleNum/BasicMain.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CompleNum
+{
+    public partial class BasicMain : Form
+    {
+        public BasicMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            Main M = new Main();
+            this.Hide();
+            M.ShowDialog();
+            this.Close();
+        }
+
+        private void BasicSolver_Click(object sender, EventArgs e)
+        {
+            QuadraticSolver BS = new QuadraticSolver();
+            this.Hide();
+            BS.ShowDialog();
+            this.Close();
+        }
+
+        private void BasicExplanation_Click(object sender, EventArgs e)
+        {
+            BasicExplantion BE = new BasicExplantion();
+            this.Hide();
+            BE.ShowDialog();
+            this.Close();
+        }
+
+        private void BasicPractice_Click(object sender, EventArgs e)
+        {
+            PracticeBasic PB = new PracticeBasic();
+            this.Hide();
+            PB.ShowDialog();
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ComplexNumers.Quadric Q = new ComplexNumers.Quadric();
+            this.Hide();
+            Q.ShowDialog();
+            this.Close();
+        }
+
+        private void Progressions_Click(object sender, EventArgs e)
+        {
+            ComplexNumers.Progression P = new ComplexNumers.Progression();
+            this.Hide();
+            P.ShowDialog();
+            this.Close();
+        }
+    }
+}
---
+++ CompleNum/CompleNum/Main.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CompleNum
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QuadraticSolver BS = new QuadraticSolver();
+            this.Hide();
+            BS.ShowDialog();
+            this.Show();
+            this.BringToFront();
+        }
+
+        private void ToBasic_Click(object sender, EventArgs e)
+        {
+            BasicMain BM = new BasicMain();
+            this.Hide();
+            BM.ShowDialog();
+            this.Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            ComplexPlane CP = new ComplexPlane();
+            this.Hide();
+            CP.ShowDialog();
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ComplexNumers.ElectricPower EP = new ComplexNumers.ElectricPower();
+            this.Hide();
+            EP.ShowDialog();
+            this.Close();
+        }
+
+
+    }
+}
